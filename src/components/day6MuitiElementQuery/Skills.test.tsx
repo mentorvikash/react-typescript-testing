48ca13c7cb6b2da9f6dadb081a769fff8229f024
mentorvikash <mentorvikash@gmail.com>
2023-07-30 11:02:49 +0530

    multi select query

---
+++ src/components/day6MuitiElementQuery/Skills.test.tsx
@@ -0,0 +1,38 @@
+import Skills from "./Skills";
+import { render, screen } from "@testing-library/react";
+
+describe("Testing skill component", () => {
+  const skills = ["painting", "singing", "dancing", "running"];
+  test("if list present", () => {
+    render(<Skills skills={skills} />);
+    const itItemPresent = screen.getByRole("list");
+    expect(itItemPresent).toBeInTheDocument();
+  });
+
+  test("all skills present", () => {
+    render(<Skills skills={skills} />);
+    const listItem = screen.getAllByRole("listitem");
+    expect(listItem).toHaveLength(skills.length);
+
+    const headingElementh1 = screen.getByRole("heading");
+    expect(headingElementh1).toBeInTheDocument();
+
+    const headingElementbyTextRegex = screen.getByText(/^LiSt Of Skills$/i); // Full text ignore case
+    expect(headingElementbyTextRegex).toBeInTheDocument();
+
+    const headingElementbyText = screen.getByText((content) =>
+      content.startsWith("List of")
+    );
+
+    expect(headingElementbyText).toBeInTheDocument();
+  });
+});
+
+// first argument to be passed in getByRole and other qeury is called textMatch
+// TextMatch could be a string, regex or function
+// screen.getByText("Hello world");  -> Full string match
+// screen.getByText("Hello world", {exact: false});  -> substring match and ignore case
+// screen.getByText(/Hello world/) -> Full match
+// screen.getByText(/Hello world/i) -> -> substring match and ignore case
+// screen.getByText(/^Hello world$/i) -> -> full string match and ignore case
+// screee.getByText((content) => content.startsWith("Hello world)) -> this will return boolean
---
+++ src/components/day6MuitiElementQuery/Skills.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { ISkills } from "./SkillsInterface";
+
+function Skills({ skills }: ISkills) {
+  return (
+    <div>
+      <h1>List of skills</h1>
+      <ul>
+        {skills.map((skill) => (
+          <li key={skill}>{skill}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Skills;
